36965edc1e640252084710173b366199b3d3f2e3
yangqibei <627643070@qq.com>
2022-05-18 18:54:42 +0800

    登录框密码框添加图标优化

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,14 +1,13 @@
-
-//! 登录注册模块 
+//! 登录注册模块
 $(function () {
-    // 点击去注册账号让 登录框隐藏，注册框显示
-    $("#link_reg").click(() => {
-      $(".login-box").hide();
-      $(".reg-box").show();
-    });
-    // 点击去登录让 注册框隐藏，登录框显示
-    $("#link_login").click(() => {
-      $(".login-box").show();
-      $(".reg-box").hide();
-    });
+  // 点击去注册账号让 登录框隐藏，注册框显示
+  $("#link_reg").click(() => {
+    $(".login-box").hide();
+    $(".reg-box").show();
   });
+  // 点击去登录让 注册框隐藏，登录框显示
+  $("#link_login").click(() => {
+    $(".login-box").show();
+    $(".reg-box").hide();
+  });
+});
