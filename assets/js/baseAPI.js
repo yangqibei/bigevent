40a8f3fae1189967c5bc98651671725b38ff9768
yangqibei <627643070@qq.com>
2022-05-19 19:07:30 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,12 +1,32 @@
-//!处理请求根路径 
+//! 5、处理请求根路径
 // 注意：每次调用 $.get() 或 $.post() 或 $.ajax() 的时候，
 // 会先调用 ajaxPrefilter 这个函数
 // 在这个函数中，可以拿到我们给Ajax提供的配置对象
 $.ajaxPrefilter((option) => {
-    // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
-    option.url = `http://www.liulongbin.top:3007` + option.url;
-  });
-  
-  
-  
-  
+  // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
+  option.url = `http://www.liulongbin.top:3007` + option.url;
+
+  // 统一为有权限的接口，设置 headers 请求头
+  if (option.url.indexOf("/my/")) {
+    option.headers = {
+      Authorization: localStorage.getItem("token"),
+    };
+  }
+
+//!给全局统一挂载complete回调函数 
+    // 不论成功还是失败，最终都会调用 complete 回调函数
+    option.complete= (res) => {
+      console.log('执行了complete回调');
+      console.log(res);
+      // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
+      if (
+        res.responseJSON.status === 1 &&
+        res.responseJSON.message === "身份认证失败！"
+      ) {
+        // 1、 强制清空 token
+        localStorage.removeItem("token");
+        // 2、 强制跳转到登录页面
+        location.href = "/login.html";
+      }
+}
+});
