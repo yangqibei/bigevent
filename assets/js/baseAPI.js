ca5fdce1a379a389ef9f2e89a1f47ad5d9d837a4
yangqibei <627643070@qq.com>
2022-05-20 14:18:17 +0800

    完成个人中心的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -13,20 +13,20 @@ $.ajaxPrefilter((option) => {
     };
   }
 
-//!给全局统一挂载complete回调函数 
-    // 不论成功还是失败，最终都会调用 complete 回调函数
-    option.complete= (res) => {
-      console.log('执行了complete回调');
-      console.log(res);
-      // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
-      if (
-        res.responseJSON.status === 1 &&
-        res.responseJSON.message === "身份认证失败！"
-      ) {
-        // 1、 强制清空 token
-        localStorage.removeItem("token");
-        // 2、 强制跳转到登录页面
-        location.href = "/login.html";
-      }
-}
+  //!给全局统一挂载complete回调函数
+  // 不论成功还是失败，最终都会调用 complete 回调函数
+  option.complete = (res) => {
+    // console.log('执行了complete回调');
+    // console.log(res);
+    // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
+    if (
+      res.responseJSON.status === 1 &&
+      res.responseJSON.message === "身份认证失败！"
+    ) {
+      // 1、 强制清空 token
+      localStorage.removeItem("token");
+      // 2、 强制跳转到登录页面
+      location.href = "/login.html";
+    }
+  };
 });
